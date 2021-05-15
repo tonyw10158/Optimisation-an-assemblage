049b7852273d897999436abff8a47dd9cf848b1b
Tony W <58774448+tonyw10158@users.noreply.github.com>
2021-05-16 04:05:15 +1200

    Create Tabu Search.py

---
+++ Tabu Search.py
@@ -0,0 +1,48 @@
+def get_neighbors(graph, solution, tabu, tenure, neighbors=4):
+    switch = None
+    switch_rev = None
+    candidate = np.array(solution)
+    tabu_dict = dict()
+    solution_distance = calculate_distance(graph, solution)
+    for i in range(neighbors):
+        found = False
+        while found == False:
+            candidate_copy = np.copy(solution)
+            switch = tuple(np.random.choice(range(1, len(graph)), 2, replace=False))
+            switch_rev = (switch[1], switch[0])
+            candidate_copy[switch[0]], candidate_copy[switch[1]] = candidate_copy[switch[1]], candidate_copy[switch[0]]
+            candidate_distance = calculate_distance(graph, candidate_copy)
+            if tabu[switch] < 1 and tabu[switch_rev] < 1:
+                tabu_dict[switch] = candidate_distance
+                found = True
+            # Aspiration criteria; if the index is in tabu but the solution increases then accept the solution.
+            elif tabu[switch] <= (tenure / 2) and tabu[switch_rev] <= (tenure / 2) and candidate_distance < solution_distance:
+                tabu_dict[switch] = candidate_distance
+                found = True
+    best = sorted(tabu_dict, key=tabu_dict.get)[0]
+    candidate[best[0]], candidate[best[1]] = candidate[best[1]], candidate[best[0]]
+    switch = best
+    switch_rev = (best[1], best[0])
+    return candidate, switch, switch_rev
+  
+  def update_tabu(tabu, switch, switch_rev, tenure=3):
+    tabu_copy = np.copy(tabu)
+    tabu_copy -= 1
+    tabu_copy[switch, switch_rev] = tenure
+    return tabu_copy
+  
+  def tabu_search(graph, iterations=500, neighbors=15, tenure=10, init_method='random'):
+    current = None
+    if init_method == 'greedy':
+        current = initial_solution_greedy(graph)
+    else:
+        current = initial_solution_random(graph)
+    tabu_matrix = np.zeros(graph.shape)
+    route = []
+    route.append(calculate_distance(graph, current))
+    for i in range(iterations):
+        candidate, switch, switch_rev = get_neighbors(graph, current, tabu_matrix, neighbors)
+        tabu_matrix = update_tabu(tabu_matrix, switch, switch_rev, tenure)
+        print(candidate, calculate_distance(graph, candidate))
+        route.append(calculate_distance(graph, candidate))
+    return route
